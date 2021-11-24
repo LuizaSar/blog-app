26d701f3b2f91ffda8b11811f000793093f21bd4
Luiza <Luiza@MacBook-Air-Hamzat.local>
2021-11-24 21:03:43 +0300

    added gitignore file for api

--- api/index.js
+++ api/index.js
@@ -13,7 +13,7 @@ dotenv.config()
 app.use(express.json())
 app.use("/images", express.static(path.join(__dirname, "/images")));
 
-
+ 
 mongoose
   .connect(process.env.MONGO_URL)
   .then(console.log("Connected to MongoDB"))
